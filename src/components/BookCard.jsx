cc07ba0f09086a360b148717943d45fb3bd2635c
Lochan <110106642+Lochan7JK@users.noreply.github.com>
2026-02-26 22:11:06 +0530

    Update BookCard.jsx

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -6,7 +6,6 @@ import { ChevronDown, Eye, SquarePen, Trash } from 'lucide-react';
 import DefaultCover from "../assets/default-book-cover.png";
 import PlaceholderCover from "./PlaceholderCover";
 
-// export default function BookCard({ book, deleteBook }) {
 export default function BookCard({ book, requestDelete, updateRating, updateStatus }) {
 
   const navigate = useNavigate();
@@ -18,7 +17,6 @@ export default function BookCard({ book, requestDelete, updateRating, updateStat
   const statusStyles = {
     reading: "bg-blue-100 text-blue-700 border border-blue-300",
     finished: "bg-emerald-100 text-emerald-700 border border-emerald-300",
-    // planned: "bg-amber-100 text-amber-700",
   };
 
 
@@ -28,20 +26,8 @@ export default function BookCard({ book, requestDelete, updateRating, updateStat
      className="bg-white rounded-xl shadow-sm border border-stone-200 p-4 flex gap-4 hover:shadow-md transition">
       
       {/* Cover */}
-      {/* <div className="w-24 h-32 bg-amber-100 rounded-lg flex-shrink-0" /> */}
       <div className="w-27 h-40 rounded-lg flex-shrink-0 overflow-hidden bg-[#fef7f2]">
-        {/* {book.coverUrl ? (
-           <img
-              src={book.coverUrl || defaultCover}
-              alt={book.title}
-              className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
-              onError={(e) => {
-                e.target.src = defaultCover;
-              }}
-          />
-        ) : 
-        <PlaceholderCover title={book.title} /> } */}
-
+  
         {hasCover ? (
             <img
               src={book.coverUrl || DefaultCover}
@@ -77,9 +63,6 @@ export default function BookCard({ book, requestDelete, updateRating, updateStat
             <p className="text-sm text-stone-500">
               by {book.author}
             </p>
-            {/* <p className="text-xs text-stone-400 mt-0.5">
-              Added {formatRelativeTime(book.createdAt)}
-            </p> */}
             <p className="text-xs text-stone-400 mt-1">
               {book.updatedAt
                 ? `Updated ${formatRelativeTime(book.createdAt, book.updatedAt)}`
@@ -87,18 +70,6 @@ export default function BookCard({ book, requestDelete, updateRating, updateStat
             </p>
 
           </div>
-
-          {/* <StatusBadge status={book.status} /> */}
-          
-          {/* <select
-            value={book.status}
-            
-            onChange={(e) => updateStatus(book.id, e.target.value)}
-            className="text-xs border border-stone-300 rounded-md px-2 py-1"
-          >
-            <option>Reading</option>
-            <option>Finished</option>
-          </select> */}
 
           <div className="relative inline-block">
             <select
@@ -113,11 +84,7 @@ export default function BookCard({ book, requestDelete, updateRating, updateStat
             >
               <option value="reading"  className={`${statusStyles.reading}`} >Reading</option>
               <option value="finished" className={`${statusStyles.finished}`}>Finished</option>
-              {/* <option value="planned">Planned</option> */}
             </select>
-            {/* <span className="absolute right-1 top-2 text-xs">
-              <ChevronDown size={12} />
-            </span> */}
           </div>
 
 
@@ -152,16 +119,7 @@ export default function BookCard({ book, requestDelete, updateRating, updateStat
               Edit
             </button>
 
-
-            {/* <button
-              onClick={() => deleteBook(book.id)}
-              className="text-red-600 text-sm px-2 hover:opacity-70"
-            >
-              Delete
-            </button> */}
-
             <button
-              // onClick={requestDelete}
               onClick={(e) => {
                 e.stopPropagation();
                 requestDelete();
@@ -178,65 +136,3 @@ export default function BookCard({ book, requestDelete, updateRating, updateStat
     </div>
   );
 }
-
-// export default function BookCard() {
-//   const book = {
-//     title: "The Gardener and the Carpenter",
-//     author: "Alison Gopnik",
-//     notes:
-//       "Great philosophy of parenting, from a grandmother who is a wise professor of philosophy and a developmental psychologist. Such a beautiful mindset and outlook...",
-//     status: "Finished",
-//     rating: 3,
-//   };
-
-//   return (
-//     <div className="bg-white rounded-xl shadow-sm border border-stone-200 p-4 flex gap-4 max-w-3xl">
-      
-//       {/* Cover Placeholder */}
-//       <div className="w-24 h-32 bg-amber-100 rounded-lg flex-shrink-0" />
-
-//       {/* Content */}
-//       <div className="flex-1">
-        
-//         {/* Header */}
-//         <div className="flex justify-between items-start">
-//           <div>
-//             <h3 className="font-semibold text-lg leading-tight">
-//               {book.title}
-//             </h3>
-//             <p className="text-sm text-stone-500">
-//               by {book.author}
-//             </p>
-//           </div>
-
-//           <StatusBadge status={book.status} />
-//         </div>
-
-//         {/* Notes */}
-//         <p className="text-sm text-stone-600 mt-2 line-clamp-2">
-//           {book.notes}
-//         </p>
-
-//         {/* Footer Row */}
-//         <div className="mt-3 flex items-center justify-between">
-          
-//           <StarRating rating={book.rating} />
-
-//           <div className="flex gap-2">
-//             <button className="bg-amber-900 text-white text-sm px-3 py-1.5 rounded-lg hover:bg-amber-800 transition">
-//               Read
-//             </button>
-
-//             <button className="border border-stone-300 text-sm px-3 py-1.5 rounded-lg hover:bg-stone-100 transition">
-//               Edit
-//             </button>
-
-//             <button className="text-red-600 text-sm px-2 hover:opacity-70">
-//               Delete
-//             </button>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
